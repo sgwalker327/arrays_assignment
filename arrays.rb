63c9b3871db52bb855c70f536ce45711c0e0c8c9
Sam Walker <sg.walker327@gmail.com>
2022-11-02 13:38:48 -0600

    Add arrays

---
+++ arrays.rb
@@ -0,0 +1,4 @@
+escape_room_employees = ["Sam", "Zach", "Jathen", "Spuds"]
+days_worked = [4, 4, 2, 3]
+average_guests_per_room = [2.33, 3.50, 1.33, 4.66]
+employee_can_run_multiple_escape_rooms = [true, true, false, true]
