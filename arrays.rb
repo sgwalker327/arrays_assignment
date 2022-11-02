d5b0e4165b71d0f6db42f46be2dc90b7481bb34c
Sam Walker <sg.walker327@gmail.com>
2022-11-02 13:49:07 -0600

    Demonstrate methods

--- arrays.rb
+++ arrays.rb
@@ -2,3 +2,18 @@ escape_room_employees = ["Sam", "Zach", "Jathen", "Spuds"]
 days_worked = [4, 4, 2, 3]
 average_guests_per_room = [2.33, 3.50, 1.33, 4.66]
 employee_can_run_multiple_escape_rooms = [true, true, false, true]
+
+# This method removes the last element in the array escape_room_employees
+escape_room_employees.pop
+
+# This method will simultaneously retrieve an array while removing the first element
+# of the days_worked array
+days_worked.shift
+
+# This method will retrieve the number of elements in the array
+# average_guests_per_room
+average_guests_per_room.length
+
+# This method will add a new element to the beginning of the employee_can_run_multiple_escape_rooms array.
+employee_can_run_multiple_escape_rooms.unshift(false)
+
